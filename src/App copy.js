0245428f67f4110a4117305b767f1ee7eb37e5c4
unknown <md40revel@gmail.com>
2023-08-19 14:22:49 +0100

    creation du datagrid et ajout de filtre aux colonnes et btn deselection

---
+++ src/App copy.js
@@ -0,0 +1,34 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import React, { useState, useRef, useEffect, useMemo, useCallback} from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
